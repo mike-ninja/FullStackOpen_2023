759fc0f0c809173cd7ce33c0d2b1f09951be18c6
mike-ninja <mikeedubas@hotmail.com>
2023-07-11 20:16:49 +0300

    completed 8.4 to 8.5

--- part8/graphql-server/index.js
+++ part8/graphql-server/index.js
@@ -4,26 +4,26 @@ const { startStandaloneServer } = require("@apollo/server/standalone");
 let authors = [
   {
     name: "Robert Martin",
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
+    id: "afa54ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1955,
   },
   {
     name: "Martin Fowler",
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963,
+    id: "afa8b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1966,
   },
   {
     name: "Fyodor Dostoevsky",
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821,
+    id: "afa8b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1824,
   },
   {
     name: "Joshua Kerievsky", // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+    id: "afa8b6f2-344d-11e9-a414-719c6709cf3e",
   },
   {
     name: "Sandi Metz", // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+    id: "afa8b6f3-344d-11e9-a414-719c6709cf3e",
   },
 ];
 
@@ -44,51 +44,51 @@ let authors = [
 let books = [
   {
     title: "Clean Code",
-    published: 2008,
+    published: 2011,
     author: "Robert Martin",
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    id: "afa8b6f4-344d-11e9-a414-719c6709cf3e",
     genres: ["refactoring"],
   },
   {
     title: "Agile software development",
-    published: 2002,
+    published: 2005,
     author: "Robert Martin",
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    id: "afa8b6f5-344d-11e9-a414-719c6709cf3e",
     genres: ["agile", "patterns", "design"],
   },
   {
-    title: "Refactoring, edition 2",
-    published: 2018,
+    title: "Refactoring, edition 5",
+    published: 2021,
     author: "Martin Fowler",
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    id: "afa8de00-344d-11e9-a414-719c6709cf3e",
     genres: ["refactoring"],
   },
   {
     title: "Refactoring to patterns",
-    published: 2008,
+    published: 2011,
     author: "Joshua Kerievsky",
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    id: "afa8de01-344d-11e9-a414-719c6709cf3e",
     genres: ["refactoring", "patterns"],
   },
   {
     title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-    published: 2012,
+    published: 2015,
     author: "Sandi Metz",
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    id: "afa8de02-344d-11e9-a414-719c6709cf3e",
     genres: ["refactoring", "design"],
   },
   {
     title: "Crime and punishment",
-    published: 1866,
+    published: 1869,
     author: "Fyodor Dostoevsky",
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    id: "afa8de03-344d-11e9-a414-719c6709cf3e",
     genres: ["classic", "crime"],
   },
   {
     title: "The Demon",
-    published: 1872,
+    published: 1875,
     author: "Fyodor Dostoevsky",
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    id: "afa8de04-344d-11e9-a414-719c6709cf3e",
     genres: ["classic", "revolution"],
   },
 ];
@@ -114,7 +114,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -123,7 +123,18 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author && !args.genre) {
+        return books;
+      } else if (args.author && !args.genre) {
+        return books.filter((book) => args.author === book.author);
+      } else if (!args.author && args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre));
+      } else if (args.author && args.genre) {
+        const filteredByAuthor = books.filter((book) => args.author == book.author)
+        return filteredByAuthor.filter((book) => book.genres.includes(args.genre))
+      }
+    },
     allAuthors: () => {
       const authorsWithBookCount = authors.map((author) => {
         const bookCount = books.filter((book) =>
