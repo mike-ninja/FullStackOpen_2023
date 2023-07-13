87fc20f805b9841f58c73ff04770753d225252f6
mike-ninja <mikeedubas@hotmail.com>
2023-07-13 11:39:10 +0300

    completed 8.12, and removed 8.11 functionality

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,28 +1,35 @@
 import { useState } from "react";
 
-const AuthorEdit = ({ editBorn }) => {
-  const [name, setName] = useState("");
+const AuthorEdit = ({ authors, editBorn }) => {
   const [year, setYear] = useState("");
 
   const submit = (event) => {
     event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+    const name = Object.fromEntries(formData.entries()).selectedAuthor;
+
     const setBornTo = parseInt(year);
     editBorn({ variables: { name, setBornTo } });
-    setName("");
     setYear("");
   };
+
+  const authorSelect = authors.map((a) => a).filter((a) => !a.born);
+
+  if (!authorSelect[0]) {
+    return null;
+  }
 
   return (
     <div>
       <h3>Set Birthyear</h3>
       <form onSubmit={submit}>
-        <div>
-          name{" "}
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
-        </div>
+        <select name="selectedAuthor">
+          {authorSelect.map((author) => (
+            <option key={author.name} value={author.name}>{author.name}</option>
+          ))}
+        </select>
         <div>
           year{" "}
           <input
@@ -62,7 +69,7 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
-      <AuthorEdit editBorn={props.editBorn} />
+      <AuthorEdit authors={authors} editBorn={props.editBorn} />
     </div>
   );
 };
