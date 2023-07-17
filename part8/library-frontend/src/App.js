5ebff20f40bb9630c18d3b090bec960ef72964e3
mike-ninja <mikeedubas@hotmail.com>
2023-07-17 17:41:35 +0300

    completed 8.17 to 8.18

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -3,14 +3,16 @@ import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import Notification from "./components/Notification";
-import { useMutation, useQuery } from "@apollo/client";
+import { useApolloClient, useMutation, useQuery } from "@apollo/client";
 import { ADD_BOOK, EDIT_BORN, GET_ALL } from "./queries";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [page, setPage] = useState("authors");
   const [errorMessage, setErrorMessage] = useState(null);
-
   const result = useQuery(GET_ALL);
+  const client = useApolloClient();
 
   const notify = (message) => {
     setErrorMessage(message);
@@ -22,7 +24,7 @@ const App = () => {
   const [addBook] = useMutation(ADD_BOOK, {
     refetchQueries: [{ query: GET_ALL }],
     onError: (error) => {
-      const messages = error.graphQLErrors[0].extensions.error.message;
+      const messages = error.graphQLErrors[0].message;
       notify(messages);
     },
   });
@@ -38,13 +40,35 @@ const App = () => {
     return <div>Loading...</div>;
   }
 
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        <button
+          style={{ display: (token ? "" : "none") }}
+          onClick={() => setPage("add")}
+        >
+          add book
+        </button>
+        <button
+          style={{ display: (token ? "none" : "") }}
+          onClick={() => setPage("login")}
+        >
+          login
+        </button>
+        <button
+          style={{ display: (token ? "" : "none") }}
+          onClick={logout}
+        >
+          logout
+        </button>
       </div>
 
       <Notification errorMessage={errorMessage} />
@@ -58,6 +82,13 @@ const App = () => {
       <Books show={page === "books"} books={result.data.allBooks} />
 
       <NewBook show={page === "add"} addBook={addBook} />
+
+      <LoginForm
+        show={page === "login"}
+        setToken={setToken}
+        notify={notify}
+        setPage={setPage}
+      />
     </div>
   );
 };
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -43,4 +43,12 @@ export const EDIT_BORN = gql`
     }
   }
 
-`
+`;
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
+    }
+  }
+`;
